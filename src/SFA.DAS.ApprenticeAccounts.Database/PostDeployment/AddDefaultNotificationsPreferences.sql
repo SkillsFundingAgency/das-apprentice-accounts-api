9357ef09f95acc220442bd7be7cc3c7920ffdcca
Sarah <sarah.coetzee@education.gov.uk>
2022-05-04 10:44:14 +0100

    Add database tables & post deployment script

---
+++ src/SFA.DAS.ApprenticeAccounts.Database/PostDeployment/AddDefaultNotificationsPreferences.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE #TempPreference
+(
+	[PreferenceId] INT ,
+    [PreferenceMeaning] NVARCHAR(50)
+)
+
+INSERT INTO #TempPreference VALUES 
+(1,'Feedback on your training provider')
+
+SET IDENTITY_INSERT [dbo].[Preference] ON;
+
+MERGE [Preference] TARGET
+USING #TempPreference SOURCE
+ON TARGET.PreferenceId=SOURCE.PreferenceId
+WHEN MATCHED THEN
+UPDATE SET TARGET.PreferenceMeaning = SOURCE.PreferenceMeaning
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (PreferenceId,PreferenceMeaning)
+VALUES (SOURCE.PreferenceId,SOURCE.PreferenceMeaning);
+
+SET IDENTITY_INSERT [dbo].[Preference] OFF;
+DROP TABLE #TempPreference
---
+++ src/SFA.DAS.ApprenticeAccounts.Database/Tables/ApprenticePreferences.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ApprenticePreferences]
+(
+	[ApprenticeId] UNIQUEIDENTIFIER NOT NULL , 
+    [PreferenceId] INT NOT NULL, 
+    [Status] INT NOT NULL, 
+    [CreatedOn] DATETIME NOT NULL, 
+    [UpdatedOn] DATETIME NULL, 
+    CONSTRAINT [FK_ApprenticePreferences_Apprentice_ApprenticeId] FOREIGN KEY ([ApprenticeId]) REFERENCES [dbo].[Apprentice] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_ApprenticePreferences_Preference_PreferenceId] FOREIGN KEY ([PreferenceId]) REFERENCES [dbo].[Preference] ([PreferenceId]) ON DELETE CASCADE
+
+)
