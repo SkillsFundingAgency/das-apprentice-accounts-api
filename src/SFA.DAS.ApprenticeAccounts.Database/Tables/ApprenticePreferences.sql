f31caa1d79a83a91ae11ea70b353cadb10e254f7
Sarah <sarah.coetzee@education.gov.uk>
2022-08-03 14:18:35 +0100

    QF-247 Update database table

--- src/SFA.DAS.ApprenticeAccounts.Database/Tables/ApprenticePreferences.sql
+++ src/SFA.DAS.ApprenticeAccounts.Database/Tables/ApprenticePreferences.sql
@@ -5,7 +5,11 @@
     [Status] BIT NOT NULL, 
     [CreatedOn] DATETIME NOT NULL, 
     [UpdatedOn] DATETIME NULL, 
-    CONSTRAINT [FK_ApprenticePreferences_Apprentice_ApprenticeId] FOREIGN KEY ([ApprenticeId]) REFERENCES [dbo].[Apprentice] ([Id]) ON DELETE CASCADE,
-    CONSTRAINT [FK_ApprenticePreferences_Preference_PreferenceId] FOREIGN KEY ([PreferenceId]) REFERENCES [dbo].[Preference] ([PreferenceId]) ON DELETE CASCADE
-
+    CONSTRAINT [FK_ApprenticePreferences_Apprentice_ApprenticeId] FOREIGN KEY ([ApprenticeId]) REFERENCES [dbo].[Apprentice] ([Id]),
+    CONSTRAINT [FK_ApprenticePreferences_Preference_PreferenceId] FOREIGN KEY ([PreferenceId]) REFERENCES [dbo].[Preference] ([PreferenceId])
 )
+    GO
+
+    CREATE UNIQUE CLUSTERED INDEX IXU_ApprenticeIdPreferenceId
+                                ON [dbo].[ApprenticePreferences](ApprenticeId, PreferenceId)
+    GO
