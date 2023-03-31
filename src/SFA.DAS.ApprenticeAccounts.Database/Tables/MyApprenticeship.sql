6841a478c6735f42e5361f1488cff4dd7d3df935
MarkFCain <mark.cain@digital.education.gov.uk>
2023-03-31 12:22:00 +0100

    CSP-672 simplified table and index creation script

--- src/SFA.DAS.ApprenticeAccounts.Database/Tables/MyApprenticeship.sql
+++ src/SFA.DAS.ApprenticeAccounts.Database/Tables/MyApprenticeship.sql
@@ -1,6 +1,5 @@
-﻿
-CREATE TABLE [dbo].[MyApprenticeship](
-	[Id] [uniqueidentifier] NOT NULL,
+﻿CREATE TABLE [dbo].[MyApprenticeship](
+	[Id] [uniqueidentifier]  NOT NULL DEFAULT newid(),
 	[ApprenticeId] [uniqueidentifier] NOT NULL,
 	[Uln] [bigint] NULL,
 	[ApprenticeshipId] [bigint] NULL,
@@ -10,31 +9,9 @@ CREATE TABLE [dbo].[MyApprenticeship](
 	[TrainingProviderId] [bigint] NULL,
 	[TrainingProviderName] [nvarchar](200) NULL,
 	[TrainingCode] [int] NULL,
-	[TrainingCourseOption] [nvarchar](200) NULL,
-	[StandardUid] [nvarchar](20) NULL,
-	[CreatedOn] [datetime2](7) NOT NULL,
- CONSTRAINT [PK_MyApprenticeship_Id] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[MyApprenticeship] ADD  DEFAULT (newid()) FOR [Id]
-GO
-
-ALTER TABLE [dbo].[MyApprenticeship] ADD  CONSTRAINT [DF_MyApprenticeship_CreatedOn]  DEFAULT (getutcdate()) FOR [CreatedOn]
-GO
-
-
-CREATE NONCLUSTERED INDEX [IDX_MyApprenticeship_ApprenticeId] ON [dbo].[MyApprenticeship]
-(
-	[ApprenticeId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-
-CREATE NONCLUSTERED INDEX [IDX_MyApprenticeship_CreatedOn] ON [dbo].[MyApprenticeship]
-(
-	[CreatedOn] DESC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
+	[TrainingCourseOption] [nvarchar](126) NULL,
+	[StandardUId] [nvarchar](20) NULL,
+	[CreatedOn] [datetime2](7) NOT NULL DEFAULT (getutcdate()),
+ 	CONSTRAINT [PK_MyApprenticeship_Id] PRIMARY KEY (Id),
+	INDEX IX_MyApprenticeship_ApprenticeIdCreatedOn NONCLUSTERED (ApprenticeId,CreatedOn)
+	)
