025dd5f9fe9389e2dd073543e90467e5a6208ff1
MarkFCain <mark.cain@digital.education.gov.uk>
2023-04-12 11:26:31 +0100

    CSP-673 removed comments from MyApprenticeships.cs

--- src/SFA.DAS.ApprenticeAccounts/Data/Models/MyApprenticeship.cs
+++ src/SFA.DAS.ApprenticeAccounts/Data/Models/MyApprenticeship.cs
@@ -11,30 +11,12 @@ public class MyApprenticeship : Entity
         /// for Entity Framework
     }
 
-
-    //MFCMFC
-    //public MyApprenticeship(Guid id, CreateMyApprenticeshipCommand command )
-    //{
-    //    Id = id;
-    //    ApprenticeId = command.ApprenticeId;
-    //    Uln = command.Uln;
-    //    ApprenticeshipId = command.ApprenticeshipId;
-    //    EmployerName = command.EmployerName;
-    //    StartDate = command.StartDate;
-    //    EndDate = command.EndDate;
-    //    TrainingProviderId = command.TrainingProviderId;
-    //    TrainingProviderName = command.TrainingProviderName;
-    //    TrainingCode = command.TrainingCode;
-    //    StandardUId = command.StandardUId;
-
-    //}
-
     public static implicit operator MyApprenticeship(CreateMyApprenticeshipCommand command)
     { 
         return new MyApprenticeship
         { 
-            Id = command.Id,
-        ApprenticeId = command.ApprenticeId,
+            Id = command.Id, 
+            ApprenticeId = command.ApprenticeId,
             Uln = command.Uln,
             ApprenticeshipId = command.ApprenticeshipId,
             EmployerName = command.EmployerName,
