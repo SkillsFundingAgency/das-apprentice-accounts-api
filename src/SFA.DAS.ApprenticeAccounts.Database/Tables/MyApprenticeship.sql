65fbc9105b34e16139d620634d8336c86f6cee0a
Paul Graham <paul.graham@coprime.co.uk>
2023-10-09 14:15:55 +0100

    chnaged to unique index

--- src/SFA.DAS.ApprenticeAccounts.Database/Tables/MyApprenticeship.sql
+++ src/SFA.DAS.ApprenticeAccounts.Database/Tables/MyApprenticeship.sql
@@ -13,5 +13,5 @@
 	[CreatedOn] [datetime2](7) NOT NULL DEFAULT (getutcdate()),
  	CONSTRAINT [PK_MyApprenticeship_Id] PRIMARY KEY (Id),
  	CONSTRAINT FK_MyApprenticeship_Apprentice FOREIGN KEY (ApprenticeId) REFERENCES [Apprentice] (Id),
-	INDEX IX_MyApprenticeship_ApprenticeId NONCLUSTERED (ApprenticeId)
+	INDEX IXU_MyApprenticeship_ApprenticeId UNIQUE NONCLUSTERED (ApprenticeId)
 	)
