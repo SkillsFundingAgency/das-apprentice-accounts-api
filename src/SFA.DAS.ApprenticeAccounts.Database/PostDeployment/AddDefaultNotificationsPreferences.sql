b688193242488459d82cec1ee2bc9b736bcbff23
Chris Woodcock <chris@humesoftware.com>
2023-11-09 09:29:19 +0000

    Add new apprentice preference for engagement emails

--- src/SFA.DAS.ApprenticeAccounts.Database/PostDeployment/AddDefaultNotificationsPreferences.sql
+++ src/SFA.DAS.ApprenticeAccounts.Database/PostDeployment/AddDefaultNotificationsPreferences.sql
@@ -1,23 +1,21 @@
 ﻿CREATE TABLE #TempPreference
 (
-	[PreferenceId] INT ,
+    [PreferenceId] INT,
     [PreferenceMeaning] NVARCHAR(200),
-	[PreferenceHint] NVARCHAR(2000)
+    [PreferenceHint] NVARCHAR(2000)
 )
 
 INSERT INTO #TempPreference VALUES 
-(1,'Giving feedback on your training provider','Emails will be sent every 3 months.')
+(1, 'Giving feedback on your training provider', 'Emails will be sent every 3 months.'),
+(2, 'Getting help and advice about your apprenticeship', 'You''ll receive regular emails during your apprenticeship')
 
-SET IDENTITY_INSERT [dbo].[Preference] ON;
-
-MERGE [Preference] TARGET
-USING #TempPreference SOURCE
-ON TARGET.PreferenceId=SOURCE.PreferenceId
+MERGE [Preference] AS TARGET
+USING #TempPreference AS SOURCE
+ON TARGET.PreferenceId = SOURCE.PreferenceId
 WHEN MATCHED THEN
-UPDATE SET TARGET.PreferenceMeaning = SOURCE.PreferenceMeaning, TARGET.PreferenceHint = SOURCE.PreferenceHint
+    UPDATE SET TARGET.PreferenceMeaning = SOURCE.PreferenceMeaning, TARGET.PreferenceHint = SOURCE.PreferenceHint
 WHEN NOT MATCHED BY TARGET THEN 
-INSERT (PreferenceId,PreferenceMeaning,PreferenceHint)
-VALUES (SOURCE.PreferenceId,SOURCE.PreferenceMeaning, SOURCE.PreferenceHint);
+    INSERT (PreferenceId, PreferenceMeaning, PreferenceHint)
+    VALUES (SOURCE.PreferenceId, SOURCE.PreferenceMeaning, SOURCE.PreferenceHint);
 
-SET IDENTITY_INSERT [dbo].[Preference] OFF;
 DROP TABLE #TempPreference
--- src/SFA.DAS.ApprenticeAccounts.Database/Tables/Preference.sql
+++ src/SFA.DAS.ApprenticeAccounts.Database/Tables/Preference.sql
@@ -1,8 +1,6 @@
 ﻿CREATE TABLE [dbo].[Preference]
 (
-	[PreferenceId] INT NOT NULL
-					IDENTITY(1, 1)
-					PRIMARY KEY, 
-	[PreferenceMeaning] NVARCHAR(200) NOT NULL,
-	[PreferenceHint] NVARCHAR(2000) NULL
+    [PreferenceId] INT NOT NULL PRIMARY KEY, 
+    [PreferenceMeaning] NVARCHAR(200) NOT NULL,
+    [PreferenceHint] NVARCHAR(2000) NULL
 )
