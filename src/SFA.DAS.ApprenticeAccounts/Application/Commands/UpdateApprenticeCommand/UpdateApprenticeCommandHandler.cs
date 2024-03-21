02a7afb9c443675a5456d4c13a6145e9f3594b9c
Shaun Downey <Shaun.Downey@fujitsuuki.onmicrosoft.com>
2024-03-21 15:17:15 +0000

    CSP-1306 - Updated project package dependencies to remove any vulnerable versions.
    CSP1306 - Removed uneccessary white space and comments.

--- src/SFA.DAS.ApprenticeAccounts/Application/Commands/UpdateApprenticeCommand/UpdateApprenticeCommandHandler.cs
+++ src/SFA.DAS.ApprenticeAccounts/Application/Commands/UpdateApprenticeCommand/UpdateApprenticeCommandHandler.cs
@@ -1,7 +1,6 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.ApprenticeAccounts.Data;
-using SFA.DAS.ApprenticeAccounts.Data.Models;
 using SFA.DAS.ApprenticeAccounts.DTOs.Apprentice;
 using System;
 using System.Net.Mail;
@@ -12,40 +11,25 @@ namespace SFA.DAS.ApprenticeAccounts.Application.Commands.UpdateApprenticeComman
 {
     public class UpdateApprenticeCommandHandler : IRequestHandler<UpdateApprenticeCommand, bool>
     {
-
         private readonly IApprenticeContext _apprentices;
 
         private readonly ILogger<UpdateApprenticeCommandHandler> _logger;
 
         public UpdateApprenticeCommandHandler(IApprenticeContext apprenticeships, ILogger<UpdateApprenticeCommandHandler> logger)
         {
-
             _apprentices = apprenticeships;
-
             _logger = logger;
-
         }
 
-        /// <summary>
-        /// Handles the update apprentice command.
-        /// </summary>
-        /// <param name="request">The update apprentice command request.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>A task representing the asynchronous operation. True if the apprentice is updated successfully, otherwise false.</returns>
-        /// <exception cref="FluentValidation.ValidationException">Thrown when validation fails.</exception>
         public async Task<bool> Handle(UpdateApprenticeCommand request, CancellationToken cancellationToken)
         {
-
             var apprentice = await _apprentices.Find(request.ApprenticeId);
 
             if (apprentice != null)
             {
-
-                string prepatchEmail = apprentice.Email.Address;
-
-                string prepatchFirstName = apprentice.FirstName;
-
-                string prepatchLastName = apprentice.LastName;
+                string prePatchEmail = apprentice.Email.Address;
+                string prePatchFirstName = apprentice.FirstName;
+                string prePatchLastName = apprentice.LastName;
 
                 ApprenticePatchDto patch = new ApprenticePatchDto(apprentice, _logger);
 
@@ -55,55 +39,32 @@ namespace SFA.DAS.ApprenticeAccounts.Application.Commands.UpdateApprenticeComman
 
                 if (!validation.IsValid)
                 {
-
                     throw new FluentValidation.ValidationException(validation.Errors);
-
                 }
 
-                if
-                (
-
-                    !string.Equals(prepatchEmail, new MailAddress(patch.Email).Address, StringComparison.Ordinal) ||
-
-                    !string.Equals(prepatchFirstName, patch.FirstName, StringComparison.Ordinal) ||
-
-                    !string.Equals(prepatchLastName, patch.LastName, StringComparison.Ordinal)
-                
-                )
+                if(!string.Equals(prePatchEmail, new MailAddress(patch.Email).Address, StringComparison.Ordinal) ||
+                   !string.Equals(prePatchFirstName, patch.FirstName, StringComparison.Ordinal) ||
+                   !string.Equals(prePatchLastName, patch.LastName, StringComparison.Ordinal))
                 {
-
                     apprentice.UpdatedOn = DateTime.UtcNow;
-
                 }
 
-                _logger.LogInformation("{HandlerName} Apprentice Id {ApprenticeId}", 
-                    
-                    nameof(UpdateApprenticeCommandHandler), 
-                    
+                _logger.LogInformation("{HandlerName} Apprentice Id {ApprenticeId}",
+                    nameof(UpdateApprenticeCommandHandler),
                     request.ApprenticeId
-                    
                 );
 
                 return true;
-
             }
             else
             {
-
                 _logger.LogInformation("{HandlerName} Apprentice Id does not exist {ApprenticeId}",
-                    
                     nameof(UpdateApprenticeCommandHandler),
-                    
                     request.ApprenticeId
-                    
                 );
                 
                 return false;
-
             }
-
         }
-
     }
-
 }
