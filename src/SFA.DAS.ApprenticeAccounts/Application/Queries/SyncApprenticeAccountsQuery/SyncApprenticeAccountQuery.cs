cefe98e26017fc387e2147592e3a4590173a12f6
Shaun Downey <Shaun.Downey@fujitsuuki.onmicrosoft.com>
2024-03-25 16:47:57 +0000

    CSP-1307 Apprentice Details Sync: Added a new POST endpoint to relay apprentice details

---
+++ src/SFA.DAS.ApprenticeAccounts/Application/Queries/SyncApprenticeAccountsQuery/SyncApprenticeAccountQuery.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using SFA.DAS.ApprenticeAccounts.DTOs.Apprentice;
+using System;
+
+namespace SFA.DAS.ApprenticeAccounts.Application.Queries.SyncApprenticeAccountsQuery;
+
+public class SyncApprenticeAccountQuery : IRequest<ApprenticeSyncResponseDto>
+{
+    public SyncApprenticeAccountQuery()
+    {
+
+    }
+
+    public SyncApprenticeAccountQuery(DateTime? updatedSince, Guid[] apprenticeIDs)
+    {
+        UpdatedSince = updatedSince;
+        ApprenticeIDs = apprenticeIDs;
+    }
+
+    [JsonProperty("updatedSince")]
+    public DateTime? UpdatedSince { get; set; }
+
+    [JsonProperty("apprenticeIds")]
+    public Guid[] ApprenticeIDs { get; set; } = Array.Empty<Guid>();
+}
