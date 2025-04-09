8c0100fdb28c6afe88490c3b75dcc9465ed22b59
Ali Reid <ali.reid@live.co.uk>
2025-04-09 09:27:53 +0100

    update

--- src/SFA.DAS.ApprenticeAccounts.Database/Tables/ApprenticeArticle.sql
+++ src/SFA.DAS.ApprenticeAccounts.Database/Tables/ApprenticeArticle.sql
@@ -1,8 +1,4 @@
-﻿SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[ApprenticeArticle](
+﻿CREATE TABLE [dbo].[ApprenticeArticle](
     [Id] [uniqueidentifier] NOT NULL,
     [EntryId] [nvarchar](200) NOT NULL,
     [EntryTitle] [nvarchar](1000) NULL,
